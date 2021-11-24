67bf3983b6fa069374b0f399b78e34cab88a7250
nishant Shah <nishantshah@nishants-MacBook-Pro.local>
2021-11-24 20:54:11 +0530

    iap support for cross-platform

---
+++ visit.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress">
+Cypress.Commands.overwrite('visit', (originalFn, subject, ...args) => {
+    if (Cypress.config().baseUrl.indexOf('localhost') != -1) {
+        return originalFn(subject, ...args);
+    } else {
+        //for windows os
+        if (navigator.userAgent.indexOf("Win") != -1) {
+            cy.exec('curl -I ' + Cypress.config().baseUrl + ' | awk "/^location/ {split($NF, a, /[=&]/); print a[2]}"').then((client_id) => {
+                return client_id.stdout
+            }).then(client_id => {
+                cy.task('getIAPToken', {
+                    url: Cypress.config().baseUrl,
+                    cid: client_id
+                }).then((token) => {
+                    console.log(token)
+                    cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
+                        .then(() => {
+                            return originalFn(subject, ...args)
+                        })
+                })
+            })
+        }
+        //for non-windows os (mac & linux) 
+        else {
+            cy.exec('curl -I ' + Cypress.config().baseUrl + " | awk '/^location/ {split($NF, a, /[=&]/); print a[2]}'").then((client_id) => {
+                return client_id.stdout
+            }).then(client_id => {
+                cy.task('getIAPToken', {
+                    url: Cypress.config().baseUrl,
+                    cid: client_id
+                }).then((token) => {
+                    console.log(token)
+                    cy.setCookie('GCP_IAAP_AUTH_TOKEN', token)
+                        .then(() => {
+                            return originalFn(subject, ...args)
+                        })
+                })
+            })
+        }
+    }
+})
